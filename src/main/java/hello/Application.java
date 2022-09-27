7b4604234cbaf4302c3f72c6b2ce9b81542c9225
yongjun.kwon <yongjun@gsneotek.com>
2022-09-27 17:19:01 +0900

    update

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -57,8 +57,105 @@ public class Application {
 
   @PostMapping("/**")
   public String index(@RequestBody ArenaUpdate arenaUpdate) {
-   
-    return "T";
+    
+
+    System.out.println(arenaUpdate._links.self.href);
+    System.out.println(arenaUpdate.arena.dims.get(1));
+    System.out.println(arenaUpdate.arena.state.values());
+    
+    // String[] commands = new String[]{"F", "R", "L", "T"};
+    String[] commands = new String[]{"F", "T", "R", "T", "L", "T"};
+    String command = "F";
+    int i = new Random().nextInt(6);
+
+    System.out.println(arenaUpdate.arena.state.keySet());
+
+    List<String> keyList = new ArrayList<>();
+    List<Integer> arrX = new ArrayList<>();
+    List<Integer> arrY = new ArrayList<>();
+    keyList.addAll(arenaUpdate.arena.state.keySet());
+
+    for(int idx=0; idx < keyList.size(); idx++){
+
+      if(arenaUpdate._links.self.href!=keyList.get(idx)){
+        System.out.println(arenaUpdate.arena.state.get(keyList.get(idx)).x);
+        System.out.println(arenaUpdate.arena.state.get(keyList.get(idx)).y);
+        arrX.add(arenaUpdate.arena.state.get(keyList.get(idx)).x);
+        arrY.add(arenaUpdate.arena.state.get(keyList.get(idx)).y);
+        }
+    }
+
+    int myX = arenaUpdate.arena.state.get(arenaUpdate._links.self.href).x;
+    int myY = arenaUpdate.arena.state.get(arenaUpdate._links.self.href).y;
+
+    switch (arenaUpdate.arena.state.get(arenaUpdate._links.self.href).direction) {
+      case "N":
+          if(myY == 0){
+            command = "R";
+          }
+          for(int idx=0; idx < arrY.size(); idx++){
+            if(myY-arrY.get(idx) > 0 && myY-arrY.get(idx) < 3){
+              command = "T";
+            }else if(myY == 0){
+              command = "R";
+            }else{
+              command = "F";
+            }
+          }
+        break;
+
+      case "W":
+          if(myX == 0){
+            command = "R";
+          }else{
+            command = "F";
+          }
+          for(int idx=0; idx < arrX.size(); idx++){
+            if(myX-arrX.get(idx) > 0 && myX-arrX.get(idx) < 3){
+              command = "T";
+            }
+          }
+        break;
+
+
+      case "S":
+
+          if(myY == arenaUpdate.arena.dims.get(1)){
+            command = "L";
+          }else{
+            command = "F";
+          }
+
+          for(int idx=0; idx < arrY.size(); idx++){
+            if(myY-arrY.get(idx) < 0 && myY-arrY.get(idx) > -3){
+              command = "T";
+            }
+          }
+        break;
+
+
+      case "E":
+        if(myX == arenaUpdate.arena.dims.get(0)){
+          command = "R";
+        }else{
+          command = "F";
+        }
+
+        for(int idx=0; idx < arrX.size(); idx++ ){
+          if(myX-arrX.get(idx) < 0 && myX-arrX.get(idx) > -3){
+            command = "T";
+          }
+        }
+
+        break;
+
+      default:
+        command = "F";
+        break;
+    }
+    // TODO add your implementation here to replace the random response. 
+    // return commands[i];
+    return command;
   }
 
 
