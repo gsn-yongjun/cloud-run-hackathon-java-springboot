2f72a057982d3d18936278843fd2d1df44168f23
yongjun.kwon <yongjun@gsneotek.com>
2022-09-27 14:41:10 +0900

    update

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -58,36 +59,6 @@ public class Application {
   public String index(@RequestBody ArenaUpdate arenaUpdate) {
     
 
-    System.out.println(arenaUpdate._links.self.href);
-    System.out.println(arenaUpdate.arena.dims);
-    System.out.println(arenaUpdate.arena.state.values());
-    System.out.println(arenaUpdate.arena.state.get("https://foo.com").x);
-    
-    // String[] commands = new String[]{"F", "R", "L", "T"};
-    String[] commands = new String[]{"F", "T", "R", "T", "L", "T"};
-    String command = "F";
-    int i = new Random().nextInt(6);
-
-    switch (arenaUpdate.arena.state.get(arenaUpdate._links.self.href).direction) {
-      case "N":
-        System.out.println("North");
-        break;
-      case "W":
-        
-        break;
-      case "S":
-        
-        break;
-      case "E":
-        
-        break;
-    
-      default:
-
-        break;
-    }
-    // TODO add your implementation here to replace the random response. 
-    // return commands[i];
     return "T";
   }
 
