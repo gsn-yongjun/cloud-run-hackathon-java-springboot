095c6cacd597f04984b2e915a9510a6c5749aefd
yongjun.kwon <yongjun@gsneotek.com>
2022-12-09 00:08:43 +0900

    modified

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,163 +1,183 @@
 package hello;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
-  static class Self {
-    public String href;
-  }
+	static class Self {
+		public String href;
+	}
 
-  static class Links {
-    public Self self;
-  }
+	static class Links {
+		public Self self;
+	}
 
-  static class PlayerState {
-    public Integer x;
-    public Integer y;
-    public String direction;
-    public Boolean wasHit;
-    public Integer score;
-  }
+	static class PlayerState {
+		public Integer x;
+		public Integer y;
+		public String direction;
+		public Boolean wasHit;
+		public Integer score;
+	}
 
-  static class Arena {
-    public List<Integer> dims;
-    public Map<String, PlayerState> state;
-  }
+	static class Arena {
+		public List<Integer> dims;
+		public Map<String, PlayerState> state;
+	}
 
-  static class ArenaUpdate {
-    public Links _links;
-    public Arena arena;
-  }
+	static class ArenaUpdate {
+		public Links _links;
+		public Arena arena;
+	}
 
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
-  @InitBinder
-  public void initBinder(WebDataBinder binder) {
-    binder.initDirectFieldAccess();
-  }
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.initDirectFieldAccess();
+	}
 
-  @GetMapping("/")
-  public String index() {
-    return "Let the battle begin!";
-  }
-  
+	@GetMapping("/")
+	public String index() {
+		return "Let the battle begin!";
+	}
 
-  @PostMapping("/**")
-  public String index(@RequestBody ArenaUpdate arenaUpdate) {
-    
+	@PostMapping("/**")
+	public String index(@RequestBody ArenaUpdate arenaUpdate) {
+		if (canThrow(arenaUpdate)) {
+			return "T";
+		} else {
+			return move(arenaUpdate);
+		}
+	}
 
-    System.out.println(arenaUpdate._links.self.href);
-    System.out.println(arenaUpdate.arena.dims.get(1));
-    System.out.println(arenaUpdate.arena.state.values());
-    
-    // String[] commands = new String[]{"F", "R", "L", "T"};
-    String[] commands = new String[]{"F", "T", "R", "T", "L", "T"};
-    String command = "F";
-    int i = new Random().nextInt(6);
+	private String move(ArenaUpdate arenaUpdate) {
+		if (areEnemiesToTheLeft(arenaUpdate)) {
+			return "L";
+		}
+		if (areEnemiesToTheRight(arenaUpdate)) {
+			return "R";
+		}
+		if (areEnemiesToTheBack(arenaUpdate)) {
+			return "L";
+		} else {
+			return "F";
+		}
+	}
 
-    System.out.println(arenaUpdate.arena.state.keySet());
+	private boolean areEnemiesToTheBack(ArenaUpdate arenaUpdate) {
 
-    List<String> keyList = new ArrayList<>();
-    List<Integer> arrX = new ArrayList<>();
-    List<Integer> arrY = new ArrayList<>();
-    keyList.addAll(arenaUpdate.arena.state.keySet());
+		PlayerState self = arenaUpdate.arena.state.entrySet().stream()
+				.filter(x -> x.getKey().equals(arenaUpdate._links.self.href)).findFirst().get().getValue();
 
-    for(int idx=0; idx < keyList.size(); idx++){
+		return arenaUpdate.arena.state.entrySet().stream().filter(x -> !x.getKey().equals(arenaUpdate._links.self.href))
+				.anyMatch(x -> isEnemyToTheBack(self, x.getValue()));
+	}
 
-      if(arenaUpdate._links.self.href!=keyList.get(idx)){
-        System.out.println(arenaUpdate.arena.state.get(keyList.get(idx)).x);
-        System.out.println(arenaUpdate.arena.state.get(keyList.get(idx)).y);
-        arrX.add(arenaUpdate.arena.state.get(keyList.get(idx)).x);
-        arrY.add(arenaUpdate.arena.state.get(keyList.get(idx)).y);
-        }
-    }
+	private boolean isEnemyToTheBack(PlayerState self, PlayerState opponent) {
 
-    int myX = arenaUpdate.arena.state.get(arenaUpdate._links.self.href).x;
-    int myY = arenaUpdate.arena.state.get(arenaUpdate._links.self.href).y;
+		switch (self.direction) {
+		case "N":
+			return self.x == opponent.x && self.y < opponent.y && self.y + 3 >= opponent.y;
+		case "S":
+			return self.x == opponent.x && self.y > opponent.y && self.y - 3 <= opponent.y;
+		case "E":
+			return self.x > opponent.x && self.x + 3 <= opponent.x && self.y == opponent.y;
+		case "W":
+			return self.x < opponent.x && self.x + 3 >= opponent.x && self.y == opponent.y;
+		default:
+			System.out.println("WARNING unknown direction: " + self.direction);
+			return false;
+		}
+	}
 
-    switch (arenaUpdate.arena.state.get(arenaUpdate._links.self.href).direction) {
-      case "N":
-          if(myY == 0){
-            command = "R";
-          }
-          for(int idx=0; idx < arrY.size(); idx++){
-            if(myY-arrY.get(idx) > 0 && myY-arrY.get(idx) < 3){
-              command = "T";
-            }else if(myY == 0){
-              command = "R";
-            }else{
-              command = "F";
-            }
-          }
-        break;
+	private boolean areEnemiesToTheRight(ArenaUpdate arenaUpdate) {
 
-      case "W":
-          if(myX == 0){
-            command = "R";
-          }else{
-            command = "F";
-          }
-          for(int idx=0; idx < arrX.size(); idx++){
-            if(myX-arrX.get(idx) > 0 && myX-arrX.get(idx) < 3){
-              command = "T";
-            }
-          }
-        break;
+		PlayerState self = arenaUpdate.arena.state.entrySet().stream()
+				.filter(x -> x.getKey().equals(arenaUpdate._links.self.href)).findFirst().get().getValue();
 
+		return arenaUpdate.arena.state.entrySet().stream().filter(x -> !x.getKey().equals(arenaUpdate._links.self.href))
+				.anyMatch(x -> isEnemyToTheRight(self, x.getValue()));
+	}
 
-      case "S":
+	private boolean isEnemyToTheRight(PlayerState self, PlayerState opponent) {
+		switch (self.direction) {
+		case "E":
+			return self.x.equals(opponent.x) && self.y > opponent.y && self.y + 3 <= opponent.y;
+		case "W":
+			return self.x.equals(opponent.x) && self.y < opponent.y && self.y - 3 >= opponent.y;
+		case "N":
+			return self.x > opponent.x && self.x + 3 <= opponent.x && self.y == opponent.y;
+		case "S":
+			return self.x > opponent.x && self.x - 3 <= opponent.x && self.y == opponent.y;
+		default:
+			System.out.println("WARNING unknown direction: " + self.direction);
+			return false;
+		}
+	}
 
-          if(myY == arenaUpdate.arena.dims.get(1)){
-            command = "L";
-          }else{
-            command = "F";
-          }
+	private boolean areEnemiesToTheLeft(ArenaUpdate arenaUpdate) {
+		PlayerState self = arenaUpdate.arena.state.entrySet().stream()
+				.filter(x -> x.getKey().equals(arenaUpdate._links.self.href)).findFirst().get().getValue();
 
-          for(int idx=0; idx < arrY.size(); idx++){
-            if(myY-arrY.get(idx) < 0 && myY-arrY.get(idx) > -3){
-              command = "T";
-            }
-          }
-        break;
+        System.out.println("areEnemiesToTheLeft" + self);
 
+		return arenaUpdate.arena.state.entrySet().stream().filter(x -> !x.getKey().equals(arenaUpdate._links.self.href))
+				.anyMatch(x -> isEnemyToTheLeft(self, x.getValue()));
+	}
 
-      case "E":
-        if(myX == arenaUpdate.arena.dims.get(0)){
-          command = "R";
-        }else{
-          command = "F";
-        }
+	private boolean isEnemyToTheLeft(PlayerState self, PlayerState opponent) {
+		switch (self.direction) {
+		case "E":
+			return self.x.equals(opponent.x) && self.y > opponent.y && self.y - 3 <= opponent.y;
+		case "W":
+			return self.x.equals(opponent.x) && self.y < opponent.y && self.y + 3 >= opponent.y;
+		case "N":
+			return self.x > opponent.x && self.x - 3 <= opponent.x && self.y == opponent.y;
+		case "S":
+			return self.x > opponent.x && self.x + 3 <= opponent.x && self.y == opponent.y;
+		default:
+			System.out.println("WARNING unknown direction: " + self.direction);
+			return false;
+		}
+	}
 
-        for(int idx=0; idx < arrX.size(); idx++ ){
-          if(myX-arrX.get(idx) < 0 && myX-arrX.get(idx) > -3){
-            command = "T";
-          }
-        }
+	private boolean canThrow(ArenaUpdate arenaUpdate) {
+		PlayerState self = arenaUpdate.arena.state.entrySet().stream()
+				.filter(x -> x.getKey().equals(arenaUpdate._links.self.href)).findFirst().get().getValue();
 
-        break;
+		return arenaUpdate.arena.state.entrySet().stream().filter(x -> !x.getKey().equals(arenaUpdate._links.self.href))
+				.anyMatch(x -> isInFront(self, x.getValue()));
+	}
 
-      default:
-        command = "F";
-        break;
-    }
-    // TODO add your implementation here to replace the random response. 
-    // return commands[i];
-    return command;
-  }
-
+	private boolean isInFront(PlayerState self, PlayerState opponent) {
+		switch (self.direction) {
+		case "N":
+			return self.x == opponent.x && self.y > opponent.y && self.y - 3 <= opponent.y;
+		case "S":
+			return self.x == opponent.x && self.y < opponent.y && self.y + 3 >= opponent.y;
+		case "W":
+			return self.x > opponent.x && self.x - 3 <= opponent.x && self.y == opponent.y;
+		case "E":
+			return self.x > opponent.x && self.x + 3 <= opponent.x && self.y == opponent.y;
+		default:
+			System.out.println("WARNING unknown direction: " + self.direction);
+			return false;
+		}
+	}
 
 }
-
