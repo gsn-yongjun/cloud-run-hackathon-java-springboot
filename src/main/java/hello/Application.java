05d19f1e7f2a0c8b84bec403c1d2202b74206ee1
yongjun.kwon <yongjun@gsneotek.com>
2022-12-09 00:46:01 +0900

    modified

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 @RestController
@@ -27,6 +26,16 @@ public class Application {
 		public String direction;
 		public Boolean wasHit;
 		public Integer score;
+
+		PlayerState(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			return obj instanceof PlayerState && ((PlayerState) obj).x == x && ((PlayerState) obj).y == y;
+		}
 	}
 
 	static class Arena {
@@ -38,78 +47,6 @@ public class Application {
 		public Links _links;
 		public Arena arena;
 	}
-
-	class Vector2 {
-		public Vector2(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-
-		public int x;
-		public int y;
-	}
-
-	class Direction {
-
-		public String direction;
-
-		Direction(String direction) {
-			this.direction = direction;
-		}
-
-		public String forward() {
-			return this.direction;
-		}
-
-		public String left() {
-			String result = null;
-
-			switch (this.direction) {
-			case "N":
-				result = "W";
-				break;
-			case "W":
-				result = "S";
-				break;
-			case "E":
-				result = "N";
-				break;
-			case "S":
-				result = "E";
-				break;
-			default:
-			}
-
-			return result;
-		}
-
-		public String right() {
-			String result = null;
-
-			switch (this.direction) {
-			case "N":
-				result = "E";
-				break;
-			case "W":
-				result = "N";
-				break;
-			case "E":
-				result = "S";
-				break;
-			case "S":
-				result = "W";
-				break;
-			default:
-			}
-
-			return result;
-		}
-
-	}
-
-	Vector2 arenaDim = null;
-	String[][] arenaMap = null;
-	boolean debug = false;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -127,183 +64,231 @@ public class Application {
 
 	@PostMapping("/**")
 	public String index(@RequestBody ArenaUpdate arenaUpdate) {
-
-		String myURL = arenaUpdate._links.self.href;
-		if (debug) {
-			System.out.println("---\nMy URL : " + myURL);
+		System.out.println(arenaUpdate);
+		String[] commands = new String[] { "F", "R", "L", "T" };
+		String me = arenaUpdate._links.self.href;
+		PlayerState meState = arenaUpdate.arena.state.get(me);
+		int vector = 1;
+		String resultCmd = "T";
+		switch (meState.direction) {
+		case "N":
+			vector = 1;
+			break;
+		case "E":
+			vector = 2;
+			break;
+		case "S":
+			vector = 3;
+			break;
+		case "W":
+			vector = 4;
 		}
-
-		Integer[] dims = arenaUpdate.arena.dims.toArray(new Integer[2]);
-		arenaDim = new Vector2(dims[0], dims[1]);
-
-		arenaMap = new String[arenaDim.x][arenaDim.y];
-
-		PlayerState me = null;
-
-		// Build map
-		int playerCount = 0;
-		for (Map.Entry<String, PlayerState> state : arenaUpdate.arena.state.entrySet()) {
-			String url = state.getKey();
-			boolean isMe = url.equals(myURL);
-			PlayerState playerState = state.getValue();
-			if (debug) {
-				System.out.println("Player " + playerCount + " (me " + isMe + ") URL " + url + " pos" + playerState.x
-						+ "," + playerState.y);
-			}
-
-			arenaMap[playerState.x][playerState.y] = url;
-
-			if (isMe) {
-				me = playerState;
-			}
-
-			playerCount++;
+		int maxBotsInRange = 0;
+		int botsInRange = findBotsInRange(meState, arenaUpdate, vector);
+		if (maxBotsInRange < botsInRange) {
+			maxBotsInRange = botsInRange;
 		}
-
-		if (me == null) {
-			System.out.println("My URL not found in player list " + myURL);
-			return "T";
+		int leftBotsInRange = findBotsInRange(meState, arenaUpdate, turnLeft(vector));
+		if (maxBotsInRange < leftBotsInRange) {
+			maxBotsInRange = leftBotsInRange;
 		}
-
-		if (me.wasHit) {
-			// Evasive action
-
-			// Clear path ahead?
-			if (evade(me, me.direction)) {
-				return "F";
-			} else if (evade(me, new Direction(me.direction).right())) {
-				return "R";
+		int rightBotsInRange = findBotsInRange(meState, arenaUpdate, turnRight(vector));
+		if (maxBotsInRange < rightBotsInRange) {
+			maxBotsInRange = rightBotsInRange;
+		}
+		int backBotsInRange = findBotsInRange(meState, arenaUpdate, turnLeft(turnLeft(vector)));
+		if (maxBotsInRange < backBotsInRange) {
+			maxBotsInRange = backBotsInRange;
+		}
+		if (maxBotsInRange > 0) {
+			if (maxBotsInRange == botsInRange) {
+				resultCmd = "T";
+			} else if (maxBotsInRange == leftBotsInRange) {
+				resultCmd = "L";
+			} else if (maxBotsInRange == rightBotsInRange) {
+				resultCmd = "R";
 			} else {
-				String[] commands = new String[] { "T", "L" };
-				int i = new Random().nextInt(2);
-				return commands[i];
+				resultCmd = "L";
 			}
-		}
-
-		String command = null;
-		Vector2 targetLocation = null;
-
-		targetLocation = detect(me, me.direction); // forward
-		if (targetLocation != null) {
-			command = "T";
 		} else {
-			targetLocation = detect(me, new Direction(me.direction).left());
-			if (targetLocation != null) {
-				command = "L";
-			} else {
-				targetLocation = detect(me, new Direction(me.direction).right());
-				if (targetLocation != null) {
-					command = "R";
+			// No bots in range
+			PlayerState closestBot = findClosestBot(meState, arenaUpdate);
+			int deltaX = meState.x - closestBot.x;
+			int deltaY = meState.y - closestBot.y;
+			if (deltaY == 0) {
+				if (deltaX > 3) {
+					// Closest bot is right of me
+					// move right
+					resultCmd = moveRight(meState);
 				} else {
-					// Nothing detected, free move
-					if (evade(me, me.direction)) {
-						command = "F";
-					} else if (evade(me, new Direction(me.direction).left())) {
-						command = "L";
-					} else {
-						command = "R";
-					}
-					// String[] commands = new String[]{"F", "R", "L"};
-					// int i = new Random().nextInt(3);
-					// command = commands[i];
+					// closest bot is left of me
+					// move left
+					resultCmd = moveLeft(meState);
 				}
-			}
-
-		}
-
-		if (debug) {
-			System.out.println(command);
-		}
-
-		return command;
-	}
-
-	private Vector2 detect(PlayerState me, String direction) {
-		Vector2 result = null;
-
-		Vector2[] targets = null;
-
-		// Is someone in front of me?
-		switch (direction) {
-		case "N":
-			targets = new Vector2[] { new Vector2(me.x, me.y - 1), new Vector2(me.x, me.y - 2),
-					new Vector2(me.x, me.y - 3) };
-			break;
-		case "W":
-			targets = new Vector2[] { new Vector2(me.x - 1, me.y), new Vector2(me.x - 2, me.y),
-					new Vector2(me.x - 3, me.y) };
-			break;
-		case "E":
-			targets = new Vector2[] { new Vector2(me.x + 1, me.y), new Vector2(me.x + 2, me.y),
-					new Vector2(me.x + 3, me.y) };
-			break;
-		case "S":
-			targets = new Vector2[] { new Vector2(me.x, me.y + 1), new Vector2(me.x, me.y + 2),
-					new Vector2(me.x, me.y + 3) };
-			break;
-		default:
-		}
-
-		String target = null;
-
-		// target position in bounds?
-		for (Vector2 v : targets) {
-			if (v.x >= 0 && v.y >= 0 && v.x < arenaDim.x && v.y < arenaDim.y) {
-				target = arenaMap[v.x][v.y];
-				if (target != null) {
-					if (debug) {
-						System.out.println("Target acquired: " + target);
-					}
-					result = v;
-					break;
+			} else if (deltaX == 0) {
+				// closest bot is above or below me
+				if (deltaY > 3) {
+					// Closest bot is below of me
+					// move down
+					resultCmd = moveDown(meState);
+				} else {
+					// closest bot is above of me
+					// move up
+					resultCmd = moveUp(meState);
 				}
-			}
-		}
-
-		return result;
-	}
-
-	private boolean evade(PlayerState me, String direction) {
-		boolean result = true; // ok to go that way
-
-		Vector2[] targets = null;
-
-		// Is someone in front of me?
-		switch (direction) {
-		case "N":
-			targets = new Vector2[] { new Vector2(me.x, me.y - 1), new Vector2(me.x, me.y - 2) };
-			break;
-		case "W":
-			targets = new Vector2[] { new Vector2(me.x - 1, me.y), new Vector2(me.x - 2, me.y) };
-			break;
-		case "E":
-			targets = new Vector2[] { new Vector2(me.x + 1, me.y), new Vector2(me.x + 2, me.y) };
-			break;
-		case "S":
-			targets = new Vector2[] { new Vector2(me.x, me.y + 1), new Vector2(me.x, me.y + 2) };
-			break;
-		default:
-		}
-
-		String target = null;
-
-		// target position in bounds?
-		for (Vector2 v : targets) {
-			if (v.x >= 0 && v.y >= 0 && v.x < arenaDim.x && v.y < arenaDim.y) {
-				target = arenaMap[v.x][v.y];
-				if (target != null) {
-					if (debug) {
-						System.out.println("Target acquired: " + target);
-					}
-					result = false;
-					break;
+			} else if (deltaX < deltaY) {
+				if (deltaX > 3) {
+					// Closest bot is right of me
+					// move right
+					resultCmd = moveRight(meState);
+				} else {
+					// closest bot is left of me
+					// move left
+					resultCmd = moveLeft(meState);
 				}
 			} else {
-				result = false;
+				// closest bot is above or below me
+				if (deltaY > 3) {
+					// Closest bot is below of me
+					// move down
+					resultCmd = moveDown(meState);
+				} else {
+					// closest bot is above of me
+					// move up
+					resultCmd = moveUp(meState);
+				}
 			}
 		}
+		return resultCmd;
+	}
 
+	private String moveDown(PlayerState meState) {
+		switch (meState.direction) {
+		case "N":
+			return "R";
+		case "E":
+			return "R";
+		case "S":
+			return "F";
+		case "W":
+			return "L";
+		default:
+			return "F";
+		}
+	}
+
+	private String moveUp(PlayerState meState) {
+		switch (meState.direction) {
+		case "N":
+			return "F";
+		case "E":
+			return "L";
+		case "S":
+			return "R";
+		case "W":
+			return "R";
+		default:
+			return "F";
+		}
+	}
+
+	private String moveRight(PlayerState meState) {
+		switch (meState.direction) {
+		case "N":
+			return "R";
+		case "E":
+			return "F";
+		case "S":
+			return "L";
+		case "W":
+			return "R";
+		default:
+			return "F";
+		}
+	}
+
+	private String moveLeft(PlayerState meState) {
+		switch (meState.direction) {
+		case "N":
+			return "L";
+		case "E":
+			return "R";
+		case "S":
+			return "R";
+		case "W":
+			return "F";
+		default:
+			return "F";
+		}
+	}
+
+	private PlayerState findClosestBot(PlayerState meState, ArenaUpdate arenaUpdate) {
+		Collection<PlayerState> playerStates = arenaUpdate.arena.state.values();
+		List<PlayerState> players = playerStates.stream().filter(v -> !v.equals(meState))
+				.collect(Collectors.toCollection(ArrayList::new));
+		Collections.sort(players, (o1, o2) -> {
+			int D1 = Math.abs(meState.x - ((PlayerState) o1).x) + Math.abs(meState.y - ((PlayerState) o1).y);
+			int D2 = Math.abs(meState.x - ((PlayerState) o2).x) + Math.abs(meState.y - ((PlayerState) o2).y);
+			if (D1 < D2)
+				return -1;
+			else if (D1 > D2)
+				return 1;
+			else
+				return 0;
+		});
+		return players.get(0);
+	}
+
+	private int findBotsInRange(PlayerState meState, ArenaUpdate arenaUpdate, int vector) {
+		PlayerState[] line = new PlayerState[3];
+		getLine(meState, line, vector);
+		int nLineBots = 0;
+		Collection<PlayerState> playerStates = arenaUpdate.arena.state.values();
+		for (PlayerState checkPos : line) {
+			if (playerStates.contains(checkPos)) {
+				nLineBots++;
+			}
+		}
+		return nLineBots;
+	}
+
+	private int turnLeft(int vector) {
+		int result = vector - 1;
+		if (result < 0)
+			result = 4;
 		return result;
+	}
+
+	private int turnRight(int vector) {
+		int result = vector + 1;
+		if (result > 4)
+			result = 1;
+		return result;
+	}
+
+	private void getLine(PlayerState meState, PlayerState[] line, int vector) {
+		switch (vector) {
+		case 1:
+			line[0] = new PlayerState(meState.x, meState.y - 1);
+			line[1] = new PlayerState(meState.x, meState.y - 2);
+			line[2] = new PlayerState(meState.x, meState.y - 3);
+			break;
+		case 3:
+			line[0] = new PlayerState(meState.x, meState.y + 1);
+			line[1] = new PlayerState(meState.x, meState.y + 2);
+			line[2] = new PlayerState(meState.x, meState.y + 3);
+			break;
+		case 2:
+			line[0] = new PlayerState(meState.x + 1, meState.y);
+			line[1] = new PlayerState(meState.x + 2, meState.y);
+			line[2] = new PlayerState(meState.x + 3, meState.y);
+			break;
+		case 4:
+			line[0] = new PlayerState(meState.x - 1, meState.y);
+			line[1] = new PlayerState(meState.x + 2, meState.y);
+			line[2] = new PlayerState(meState.x - 3, meState.y);
+			break;
+		}
 	}
 
 }
