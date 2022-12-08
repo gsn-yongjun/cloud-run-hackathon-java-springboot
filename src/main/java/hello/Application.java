0282775430307c39dfe2298803eec881d87ad155
yongjun.kwon <yongjun@gsneotek.com>
2022-12-09 00:39:38 +0900

    modified

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -5,11 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
-import java.util.Set;
 
 @SpringBootApplication
 @RestController
@@ -41,6 +39,78 @@ public class Application {
 		public Arena arena;
 	}
 
+	class Vector2 {
+		public Vector2(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		public int x;
+		public int y;
+	}
+
+	class Direction {
+
+		public String direction;
+
+		Direction(String direction) {
+			this.direction = direction;
+		}
+
+		public String forward() {
+			return this.direction;
+		}
+
+		public String left() {
+			String result = null;
+
+			switch (this.direction) {
+			case "N":
+				result = "W";
+				break;
+			case "W":
+				result = "S";
+				break;
+			case "E":
+				result = "N";
+				break;
+			case "S":
+				result = "E";
+				break;
+			default:
+			}
+
+			return result;
+		}
+
+		public String right() {
+			String result = null;
+
+			switch (this.direction) {
+			case "N":
+				result = "E";
+				break;
+			case "W":
+				result = "N";
+				break;
+			case "E":
+				result = "S";
+				break;
+			case "S":
+				result = "W";
+				break;
+			default:
+			}
+
+			return result;
+		}
+
+	}
+
+	Vector2 arenaDim = null;
+	String[][] arenaMap = null;
+	boolean debug = false;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
@@ -57,201 +127,183 @@ public class Application {
 
 	@PostMapping("/**")
 	public String index(@RequestBody ArenaUpdate arenaUpdate) {
-		System.out.println(arenaUpdate);
-		System.out.println("Dim: " + arenaUpdate.arena.dims.toString());
-		Map<String, PlayerState> state = arenaUpdate.arena.state;
 
-		int dim_x = arenaUpdate.arena.dims.get(0);
-		int dim_y = arenaUpdate.arena.dims.get(1);
-
-		int my_x = 0;
-		int my_y = 0;
-		String my_dir = "N";
-		boolean my_washit = false;
-
-		Set<PlayerState> nsSet = new HashSet<PlayerState>();
-		Set<PlayerState> weSet = new HashSet<PlayerState>();
-
-		System.out.println("Player count: " + state.size());
-
-		for (String str : state.keySet()) {
-			PlayerState player = state.get(str);
-			String me = "";
-			if (str.contains("so6zjyq3bq")) {
-				my_x = player.x;
-				my_y = player.y;
-				my_dir = player.direction;
-				my_washit = player.wasHit;
-				me = "ME->";
-			}
-
-			if (my_y == player.y && Math.abs(my_x - player.x) <= 3) {
-				nsSet.add(player);
-			}
-
-			if (my_x == player.x && Math.abs(my_y - player.y) <= 3) {
-				weSet.add(player);
-			}
-
-			System.out.println(me + "URL= " + str + ", dir=" + player.direction + ", x=" + player.x + ", y=" + player.y
-					+ ", washit=" + player.wasHit + ", score=" + player.score);
-		}
-		System.out.println("My stats are x,y,dir,washit: " + my_x + ", " + my_y + ", " + my_dir + ", " + my_washit);
-
-		boolean canThrow = false;
-
-		for (String str : state.keySet()) {
-
-			PlayerState player = state.get(str);
-
-			if (!str.contains("so6zjyq3bq")) {
-//    		PlayerState player = state.get(str);
-				int x1 = player.x;
-				int y1 = player.y;
-//    		double distance_sq = Math.pow( Math.abs(my_x-x1), 2) + Math.pow(Math.abs(my_y- y1), 2);
-//    		double distance = Math.sqrt(distance_sq);
-//    		System.out.println("Opponent in range, " + distance + ", " + str + " , Throw is set");
-//    		if(distance<=4) {
-//    			canThrow=true;
-//    			
-//    		}
-
-				if (my_dir.equals("E") && my_y == y1 && x1 - my_x <= 3 && (x1 > my_x)) {
-					System.out.println("Opponent in range (" + x1 + "," + y1 + "), me (" + my_x + "," + my_y + ") "
-							+ str + " , Throw is set");
-					canThrow = true;
-					break;
-				}
-				if (my_dir.equals("W") && my_y == y1 && (my_x - x1) <= 3 & (my_x > x1)) {
-					System.out.println("Opponent in range (" + x1 + "," + y1 + "), me (" + my_x + "," + my_y + ") "
-							+ str + " , Throw is set");
-					canThrow = true;
-					break;
-				}
-				if (my_dir.equals("N") && my_x == x1 && my_y - y1 <= 3 && (my_y > y1)) {
-					System.out.println("Opponent in range (" + x1 + "," + y1 + "), me (" + my_x + "," + my_y + ") "
-							+ str + " , Throw is set");
-					canThrow = true;
-					break;
-				}
-				if (my_dir.equals("S") && my_x == x1 && y1 - my_y <= 3 && (y1 > my_y)) {
-					System.out.println("Opponent in range (" + x1 + "," + y1 + "), me (" + my_x + "," + my_y + ") "
-							+ str + " , Throw is set");
-					canThrow = true;
-					break;
-				}
-			}
+		String myURL = arenaUpdate._links.self.href;
+		if (debug) {
+			System.out.println("---\nMy URL : " + myURL);
 		}
 
-		String[] commands = new String[] { "F", "R", "L", "T" };
+		Integer[] dims = arenaUpdate.arena.dims.toArray(new Integer[2]);
+		arenaDim = new Vector2(dims[0], dims[1]);
 
-		if (my_washit) {
-			String[] move_commands = new String[] { "F", "R", "L" };
-			int i = new Random().nextInt(3);
-			System.out.println("washit true, random action: " + move_commands[i]);
-			return move_commands[i];
+		arenaMap = new String[arenaDim.x][arenaDim.y];
+
+		PlayerState me = null;
+
+		// Build map
+		int playerCount = 0;
+		for (Map.Entry<String, PlayerState> state : arenaUpdate.arena.state.entrySet()) {
+			String url = state.getKey();
+			boolean isMe = url.equals(myURL);
+			PlayerState playerState = state.getValue();
+			if (debug) {
+				System.out.println("Player " + playerCount + " (me " + isMe + ") URL " + url + " pos" + playerState.x
+						+ "," + playerState.y);
+			}
+
+			arenaMap[playerState.x][playerState.y] = url;
+
+			if (isMe) {
+				me = playerState;
+			}
+
+			playerCount++;
 		}
 
-		boolean[] luck = new boolean[] { false, false, false, false, false, false, true };
-		int randLuck = new Random().nextInt(5);
-		boolean randomReturn = luck[randLuck];
-		if (randomReturn) {
-			int i = new Random().nextInt(4);
-			System.out.println("randomReturn: " + randomReturn + ", random move: " + commands[i]);
-			return commands[i];
-		} else {
-
-			System.out.println("randomReturn: " + randomReturn + ", standard logic.");
-		}
-
-		if (canThrow) {
-			System.out.println("Throw Water");
+		if (me == null) {
+			System.out.println("My URL not found in player list " + myURL);
 			return "T";
-		} else {
-
-			String move = "F";
-
-			/* look for opp in 4 directions */
-//    	if( my_dir.equals("N"))
-			for (PlayerState player : nsSet) {
-				if (my_dir.equals("N") && player.x < my_x) {
-					move = "L";
-					System.out.println("Turning L towards player (" + player.x + "," + player.y + ")");
-					break;
-				}
-				if (my_dir.equals("N") && my_x < player.x) {
-					move = "R";
-					System.out.println("Turning R towards player (" + player.x + "," + player.y + ")");
-					break;
-				}
-
-				if (my_dir.equals("S") && player.x < my_x) {
-					move = "R";
-					System.out.println("Turning R towards player (" + player.x + "," + player.y + ")");
-					break;
-				}
-				if (my_dir.equals("S") && player.x > my_x) {
-					move = "L";
-					System.out.println("Turning L towards player (" + player.x + "," + player.y + ")");
-					break;
-				}
-			}
-
-			// return only if there is a change
-			if (!move.equals("F")) {
-				return move;
-			}
-
-			/* no opponent found, default is F */
-
-			System.out.println("No opp in range, deciding next move.");
-			move = "F";
-
-			// moving Southwards
-			if (my_dir.equals("S") && dim_y - my_y <= 3) {
-				if (dim_x / 2 > my_x) {
-					move = "L";
-				} else {
-					move = "R";
-				}
-				System.out.println("[S]Move set to: " + move);
-			}
-
-			// moving Northwards
-			if (my_dir.equals("N") && my_y <= 3) {
-				if (dim_x / 2 > my_x) {
-					move = "R";
-				} else {
-					move = "L";
-				}
-				System.out.println("[N]Move set to: " + move);
-			}
-
-			// moving East
-			if (my_dir.equals("E") && dim_x - my_x <= 3) {
-				if (dim_y / 2 > my_y) {
-					move = "R";
-				} else {
-					move = "L";
-				}
-				System.out.println("[E]Move set to: " + move);
-			}
-
-			// moving West
-			if (my_dir.equals("W") && my_x <= 3) {
-				if (dim_y / 2 > my_y) {
-					move = "L";
-				} else {
-					move = "R";
-				}
-				System.out.println("[W]Move set to: " + move);
-			}
-
-			System.out.println("Final Move: " + move);
-			return move;
-//    	int i = new Random().nextInt(4);
-//    	return commands[i];
 		}
+
+		if (me.wasHit) {
+			// Evasive action
+
+			// Clear path ahead?
+			if (evade(me, me.direction)) {
+				return "F";
+			} else if (evade(me, new Direction(me.direction).right())) {
+				return "R";
+			} else {
+				String[] commands = new String[] { "T", "L" };
+				int i = new Random().nextInt(2);
+				return commands[i];
+			}
+		}
+
+		String command = null;
+		Vector2 targetLocation = null;
+
+		targetLocation = detect(me, me.direction); // forward
+		if (targetLocation != null) {
+			command = "T";
+		} else {
+			targetLocation = detect(me, new Direction(me.direction).left());
+			if (targetLocation != null) {
+				command = "L";
+			} else {
+				targetLocation = detect(me, new Direction(me.direction).right());
+				if (targetLocation != null) {
+					command = "R";
+				} else {
+					// Nothing detected, free move
+					if (evade(me, me.direction)) {
+						command = "F";
+					} else if (evade(me, new Direction(me.direction).left())) {
+						command = "L";
+					} else {
+						command = "R";
+					}
+					// String[] commands = new String[]{"F", "R", "L"};
+					// int i = new Random().nextInt(3);
+					// command = commands[i];
+				}
+			}
+
+		}
+
+		if (debug) {
+			System.out.println(command);
+		}
+
+		return command;
+	}
+
+	private Vector2 detect(PlayerState me, String direction) {
+		Vector2 result = null;
+
+		Vector2[] targets = null;
+
+		// Is someone in front of me?
+		switch (direction) {
+		case "N":
+			targets = new Vector2[] { new Vector2(me.x, me.y - 1), new Vector2(me.x, me.y - 2),
+					new Vector2(me.x, me.y - 3) };
+			break;
+		case "W":
+			targets = new Vector2[] { new Vector2(me.x - 1, me.y), new Vector2(me.x - 2, me.y),
+					new Vector2(me.x - 3, me.y) };
+			break;
+		case "E":
+			targets = new Vector2[] { new Vector2(me.x + 1, me.y), new Vector2(me.x + 2, me.y),
+					new Vector2(me.x + 3, me.y) };
+			break;
+		case "S":
+			targets = new Vector2[] { new Vector2(me.x, me.y + 1), new Vector2(me.x, me.y + 2),
+					new Vector2(me.x, me.y + 3) };
+			break;
+		default:
+		}
+
+		String target = null;
+
+		// target position in bounds?
+		for (Vector2 v : targets) {
+			if (v.x >= 0 && v.y >= 0 && v.x < arenaDim.x && v.y < arenaDim.y) {
+				target = arenaMap[v.x][v.y];
+				if (target != null) {
+					if (debug) {
+						System.out.println("Target acquired: " + target);
+					}
+					result = v;
+					break;
+				}
+			}
+		}
+
+		return result;
+	}
+
+	private boolean evade(PlayerState me, String direction) {
+		boolean result = true; // ok to go that way
+
+		Vector2[] targets = null;
+
+		// Is someone in front of me?
+		switch (direction) {
+		case "N":
+			targets = new Vector2[] { new Vector2(me.x, me.y - 1), new Vector2(me.x, me.y - 2) };
+			break;
+		case "W":
+			targets = new Vector2[] { new Vector2(me.x - 1, me.y), new Vector2(me.x - 2, me.y) };
+			break;
+		case "E":
+			targets = new Vector2[] { new Vector2(me.x + 1, me.y), new Vector2(me.x + 2, me.y) };
+			break;
+		case "S":
+			targets = new Vector2[] { new Vector2(me.x, me.y + 1), new Vector2(me.x, me.y + 2) };
+			break;
+		default:
+		}
+
+		String target = null;
+
+		// target position in bounds?
+		for (Vector2 v : targets) {
+			if (v.x >= 0 && v.y >= 0 && v.x < arenaDim.x && v.y < arenaDim.y) {
+				target = arenaMap[v.x][v.y];
+				if (target != null) {
+					if (debug) {
+						System.out.println("Target acquired: " + target);
+					}
+					result = false;
+					break;
+				}
+			} else {
+				result = false;
+			}
+		}
+
+		return result;
 	}
 
 }
